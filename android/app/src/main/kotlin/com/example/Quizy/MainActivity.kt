7b9bd4582c474164be513b7e1620d893b83ff9ac
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2021-04-30 15:46:25 +0600

    Uploading my quiz app

---
+++ android/app/src/main/kotlin/com/example/Quizy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Quizy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreIconList = [];
+  int score = 0;
+
+  Icon correctAnswer() {
+    return Icon(
+      Icons.check,
+      color: Colors.green,
+    );
+  }
+
+  Icon falseAnswer() {
+    return Icon(
+      Icons.close,
+      color: Colors.red,
+    );
+  }
+
+  void checkAnswer(bool userPickedAnswer) {
+    if (quizBrain.getQuestionAnswer() == userPickedAnswer) {
+      scoreIconList.add(correctAnswer());
+      score++;
+    } else {
+      scoreIconList.add(falseAnswer());
+    }
+
+    setState(() {
+      bool x = quizBrain.nextQuestion();
+      if (x == false) {
+        endgame();
+      }
+    });
+  }
+
+  void endgame() {
+     Alert(
+      context: context,
+      type: AlertType.success,
+      title: "Finished!",
+      desc: "Your Final Score is $score/${quizBrain.getQuestionsCount()}",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "Cancel",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {
+            setState(() {
+              scoreIconList = [];
+              quizBrain.resetCount();
+              score = 0;
+            });
+            Navigator.pop(context);
+
+          },
+          width: 120,
+        )
+      ],
+    ).show();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreIconList,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
